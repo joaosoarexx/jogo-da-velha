060f776fd16adebd9c813e93a63013aa8e639416
joaosoarexx <jvtrgmlcmsoares@gmail.com>
2025-08-25 21:20:16 -0300

    NEW: criação do tabuleiro 3x3

--- src/App.js
+++ src/App.js
@@ -5,15 +5,23 @@ import './App.css'
 export default function Square(){
   return (
     <div>
-      <button className="square">1</button>
-      <button className="square">2</button>
-      <button className="square">3</button>
-      <button className="square">4</button>
-      <button className="square">5</button>
-      <button className="square">6</button>
-      <button className="square">7</button>
-      <button className="square">8</button>
-      <button className="square">9</button>
+      <div>
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+
+      <div>
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+
+      <div>
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
     </div>
   )
 }
